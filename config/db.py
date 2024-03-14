e68289aa2f43d9faac5d88493b39fa8d57cf3c6f
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-03-14 22:31:40 +0100

    Create event for attendees

--- config/db.py
+++ config/db.py
@@ -1,33 +1,33 @@
 import time
 import os
 from pymongo import MongoClient
-# from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer, KafkaConsumer
 
 # Connecting to Database
-# conn = MongoClient("mongodb://localhost:27017")
-db_host = os.getenv("DB_HOST", "localhost")
-db_port = os.getenv("DB_PORT", 27017)
+conn = MongoClient("mongodb://localhost:27017/local")
+# db_host = os.getenv("DB_HOST", "localhost")
+# db_port = os.getenv("DB_PORT", 27017)
 
-print(f"mongodb://{db_host}:{db_port}")
+# print(f"mongodb://{db_host}:{db_port}")
 
-client = MongoClient(f"mongodb://{db_host}:{db_port}")
-db = client['calendar_db']
-events = db['event']
+# client = MongoClient(f"mongodb://{db_host}:{db_port}")
+# db = client['local']
+# events = db['event']
 
-# KAFKA_TOPIC = 'events'
-# KAFKA_SERVER = 'kafka:9092'
-#
-# def create_kafka_producer(retries=6, delay=5):
-#     for _ in range(retries):
-#         try:
-#             producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, acks='all')
-#             return producer
-#         except Exception as e:
-#             print(f"Kafka Producer connection failed. Retrying in {delay} seconds.")
-#             time.sleep(delay)
-#     raise Exception("Failed to connect to Kafka after several attempts.")
-#
-# # Use the function to create your Kafka producer
-# KAFKA_PRODUCER = create_kafka_producer()
-#
-# KAFKA_CONSUMER = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=KAFKA_SERVER, auto_offset_reset='earliest')
+KAFKA_TOPIC = 'events'
+KAFKA_SERVER = 'kafka:9092'
+
+def create_kafka_producer(retries=6, delay=5):
+    for _ in range(retries):
+        try:
+            producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, acks='all')
+            return producer
+        except Exception as e:
+            print(f"Kafka Producer connection failed. Retrying in {delay} seconds.")
+            time.sleep(delay)
+    raise Exception("Failed to connect to Kafka after several attempts.")
+
+# Use the function to create your Kafka producer
+KAFKA_PRODUCER = create_kafka_producer()
+
+KAFKA_CONSUMER = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=KAFKA_SERVER, auto_offset_reset='earliest')
--- routes/event.py
+++ routes/event.py
@@ -2,11 +2,11 @@ import datetime
 
 from bson import ObjectId
 from fastapi import APIRouter, Depends
-from config.db import events
+from config.db import conn
 from models.event import Event, Attendee, Reminder
 from schemas.event import serializeDict, serializeList
 from notification.notification import NotificationService
-# from event_store.event_store import KAFKA_TOPIC, publish_event, consume_events
+from event_store.event_store import KAFKA_TOPIC, publish_event, consume_events
 from security.auth import get_current_user
 
 event = APIRouter(prefix='/event')
@@ -16,11 +16,11 @@ notification_service = NotificationService()
 @event.get('/', tags=["Get Methods"])
 async def find_all_events(current_user: dict = Depends(get_current_user)):
     username = current_user['username']
-    return serializeList(events.find({"userID": username}))
+    return serializeList(conn.local.event.find({"userID": username}))
 
 @event.get('/{id}', tags=["Get Methods"])
 async def fine_one_event(id: str):
-    return serializeDict(events.find_one({"_id":ObjectId(id)}))
+    return serializeDict(conn.local.event.find_one({"_id":ObjectId(id)}))
 
 @event.get("/consume-logs/")
 async def consume_logs(max_messages: int = 5):
@@ -39,7 +39,7 @@ async def create_event(event: Event):
     event_params['reminders'] = reminders_dict
     event_params['attendees'] = attendees_dict
 
-    result = events.insert_one(event_params)
+    # result = conn.local.event.insert_one(event_params)
     # publish events to event store
     # publish_event("post",event_params)
 
@@ -47,23 +47,32 @@ async def create_event(event: Event):
     time_change = datetime.timedelta(minutes=event_params['reminders'][0]['timeBefore'])
     reminder_time = reminder_time - time_change
     string_time = reminder_time.strftime('%Y-%m-%dT%H:%M:%S')
+    
+    # Code to create event for all attendees
+    all_attendees = attendees_dict
+    print(all_attendees)
+    all_attendees.append({"userID": event_params["userID"] , "attending": "True"})
+    print(all_attendees)
 
-    email_params = {
-        'email': 'milankopp2@gmail.com', #TODO: get user email address from cookies
-        'subject': 'Event Reminder',
-        'body': f'You have an event in {event_params["reminders"][0]["timeBefore"]} minutes. '
-                f'Title: {event_params["title"]}, '
-                f'Description: {event_params["description"]}, '
-                f'Location: {event_params["location"]}',
-        'time': string_time
-    }
+    for attendee in all_attendees:
+        event_params["userID"] = attendee["userID"]
+        result = conn.local.event.insert_one(event_params)
+        # email_params = {
+        #     'email': attendee["userID"],
+        #     'subject': 'Event Reminder',
+        #     'body': f'You have an event in {event_params["reminders"][0]["timeBefore"]} minutes. '
+        #             f'Title: {event_params["title"]}, '
+        #             f'Description: {event_params["description"]}, '
+        #             f'Location: {event_params["location"]}',
+        #     'time': string_time
+        # }
+        # notification_service.schedule_email(email_params)
 
-    notification_service.schedule_email(email_params)
-    return serializeDict(events.find_one({"_id":result.inserted_id}))
+    return serializeDict(conn.local.event.find_one({"_id":result.inserted_id}))
 
 @event.put('/{id}', tags=["Put Methods"])
 async def update_event(id: str, event: Event):
-    update_result = events.find_one_and_update(
+    update_result = conn.local.event.find_one_and_update(
         {"_id": ObjectId(id)},
         {"$set": dict(event)}
     )
@@ -71,10 +80,10 @@ async def update_event(id: str, event: Event):
         return {"error": "Event not found"}
     # publish events to event store
     publish_event("put",dict(event))
-    return serializeDict(events.find_one({"_id": ObjectId(id)}))
+    return serializeDict(conn.local.event.find_one({"_id": ObjectId(id)}))
 
 @event.delete('/{id}', tags=["Delete Methods"])
 async def delete_event(id: str):
     # publish events to event store
     publish_event("delete",{"id": id})
-    return serializeDict(events.find_one_and_delete({"_id":ObjectId(id)}))
+    return serializeDict(conn.local.event.find_one_and_delete({"_id":ObjectId(id)}))
