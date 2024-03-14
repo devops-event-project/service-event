259f7584290d56c3e2d5baa26c05ebd3e5f33c25
Milan Kopp <m.l.kopp@students.uu.nl>
2024-03-14 21:12:30 +0100

    fixes

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ service_event = FastAPI()
 
 service_event.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://0.0.0.0:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
